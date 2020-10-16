03e97480ac4f06911c6e76361698fb379f3adcd6
Ahhye Cho <whdkgp97@khu.ac.kr>
2020-10-16 11:14:22 +0900

    modify mult.cpp

--- mult.cpp
+++ mult.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-	for (int i = 1; i <= 9; i++) { // 9단 수정
+	for (int i = 1; i <= 9; i++) { // 8 -> 9
 		cout << i << " 단" << endl;
 		for (int j = 1; j <= 9; j++) {
 			cout << i << " * " << j << " = " << i * j << endl;
